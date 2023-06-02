94bd4f6cd462d4def9fb9cf072d89dc85f45e679
DitGrau <juditgraup@gmail.com>
2023-06-02 15:26:42 +0200

    guardado del nombre del user en el session storage, js de la pagina user acabado

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 // el logo, en todas las paginas, devuelve a la home
 const navegarHome = () => window.location.href = "../index.html";
 
+// para que despues de validar si el input del nombre esta completo me redirija 
+const navegarSettings = () => window.location.href = "./settings.html";
+
+// en el input de la vista user-name guardo el dato para utilizarlo en el final del juego
+const guardarUser = () => {
+    let nombreUser = document.getElementById('nombreUser').value;
+    if (nombreUser !== ""){
+    sessionStorage.setItem('nombreUser', nombreUser);
+    navegarSettings();
+    }
+console.log (nombreUser);
+};

ac7627182a251204c10f3c547c8c38908a8dcff7
DitGrau <juditgraup@gmail.com>
2023-06-02 20:16:31 +0200

    tableros acabados, todo listo para empezar con la logica del juego

--- js/game.js
+++ js/game.js
@@ -16,11 +16,35 @@ let contenedorTablero = document.getElementById('gameBoard');
 let contenedorColoresElegidos = document.getElementById('coloresBoard');
 let contenedorCombiRandom = document.getElementById('combiRandom');
 
-const tableroJuego = () => {
+////////////////////////////////////////////////////////////// la hardcodeo y luego vere como traigo el nivel 
+const tableroJuego = (filas) => {
+    for (let i = 0; i < filas; i++) {
+        let filaTablero = document.createElement('div');
+        filaTablero.classList.add('row');
+        contenedorTablero.appendChild(filaTablero);
 
-}
-tableroJuego ();
+        for (let j = 0; j < 4; j++) {
+            let celdaTablero = document.createElement('div');
+            celdaTablero.classList.add('bolasPequeñas');
+            filaTablero.appendChild(celdaTablero);
+        }
 
+        let pistasTablero = document.createElement('div');
+        pistasTablero.classList.add('cajon');
+        filaTablero.appendChild(pistasTablero);
+
+        for (let k = 0; k < 4; k++) {
+            let pistas = document.createElement('div');
+            pistas.classList.add('pistas');
+            pistasTablero.appendChild(pistas);
+        }
+    }
+    
+    }
+
+tableroJuego (8);
+
+// Saco los colores elegidos por pantalla para que el user sepa que opciones de color tiene
 const tableroColores = () => {
     for (let i = 0; i < coloresElegidos.length; i++) {
         let bolaElegida = document.createElement('div');
@@ -31,6 +55,8 @@ const tableroColores = () => {
 }
 tableroColores();
 
+/* la combinacion secreta permanecera oculta pero la necesito para mostrarla al final del juego 
+y para obtener el color en RGB */
 const tableroRandom = () => {
     for (let i = 0; i < arrayRandom.length; i++) {
         let bolaRandom = document.createElement('div');
